ae12bd6ea3701ad6c342c895b6990c6b71cb4b52
Thirulok <thirulok.r.2020.cse@ritchennai.edu.in>
2022-08-24 22:43:28 +0530

    A little progress

--- app/src/main/java/com/techerra/infoshare/models/User.java
+++ app/src/main/java/com/techerra/infoshare/models/User.java
@@ -3,8 +3,9 @@ package com.techerra.infoshare.models;
 import java.io.Serializable;
 
 public class User implements Serializable {
-    public String name, image ,email, token;
+    public String name, image ,email, token, id;
 
     public void add(User user) {
     }
 }
+
