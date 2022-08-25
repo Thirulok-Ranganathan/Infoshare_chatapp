1f8810cc352d94f91b21713d2a33a8d8fb26dbd9
Thirulok <thirulok.r.2020.cse@ritchennai.edu.in>
2022-08-26 00:12:28 +0530

    Done_and_dusted

--- app/src/main/java/com/techerra/infoshare/utilities/Constants.java
+++ app/src/main/java/com/techerra/infoshare/utilities/Constants.java
@@ -1,5 +1,7 @@
 package com.techerra.infoshare.utilities;
 
+import java.util.HashMap;
+
 public class Constants {
     public static final String KEY_COLLECTION_USERS = "users";
     public static final String KEY_NAME = "name";
@@ -23,4 +25,24 @@ public class Constants {
     public static final String KEY_RECEIVER_IMAGE = "receiverImage";
     public static final String KEY_LAST_MESSAGE = "lastMessage";
     public static final String KEY_AVAILABILITY = "availability";
+    public static final String REMOTE_MSG_AUTHORIZATION = "Authorization";
+    public static final String REMOTE_MSG_CONTENT_TYPE = "Content-Type";
+    public static final String REMOTE_MSG_DATA = "data";
+    public static final String REMOTE_MSG_REGISTRATION_IDS = "registration_ids";
+
+    public static HashMap<String, String> remoteMsgHeaders = null;
+    public static HashMap<String, String>getRemoteMsgHeaders(){
+        if (remoteMsgHeaders == null) {
+            remoteMsgHeaders = new HashMap<>();
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_AUTHORIZATION,
+                    "key=AAAACXt3zaI:APA91bFvzPOxOoBCldfmuatTR7wr1dj_5_9b1N-4XBe1f2H5sENpSfj9-xrQ9r_IpzLv6Zrq7-A7M_3sbWBrVfcrhXPSzhSLeGWe2SDju2Bu_TuSHkt8au8uAeg_lZ3dQ7sfPpHcKWcr"
+            );
+            remoteMsgHeaders.put(
+                    REMOTE_MSG_CONTENT_TYPE,
+                    "application/json"
+            );
+        }
+        return remoteMsgHeaders;
+    }
 }
